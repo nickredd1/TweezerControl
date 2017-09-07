ccf25e2705ebaa0e7d5a94d5b9edbcea0dc68a6d
nredd <nicholaseredd@gmail.com>
2017-09-07 10:56:44 -0700

    Changed Power Spectrum FFT algorithm for CharacterizeElectronicsManager

--- Logic/Classes/CharacterizeElectronicsManager.m
+++ Logic/Classes/CharacterizeElectronicsManager.m
@@ -47,11 +47,12 @@ classdef CharacterizeElectronicsManager < GUIManager
                 'MinPeakHeight', -40);
                 data1(1,i) = 10^(pks(1)/10);
                 data1(2,i) = set1(1, i);
-                data1(2,i)
             end
-            axes(set1Plot)
+            figure
+            subplot(1,2,1) 
             plot(data1(2,:), data1(1,:))
-            
+            title(['Power (mW) vs Channel Ampltiude'...
+                '(mVp) for Low Gain Channel'])
             for i = 1:length(set2)
                 awg.changeAmplitude(set2(1, i));
                 pause(.5)
@@ -62,8 +63,14 @@ classdef CharacterizeElectronicsManager < GUIManager
                 data2(1,i) = 10^(pks(1)/10);
                 data2(2,i) = set2(1, i);
             end
-            axes(set2Plot)
+            subplot(1,2,2) 
             plot(data2(2,:), data2(1,:))
+            title(['Power (mW) vs Channel Ampltiude'...
+                '(mVp) for High Gain Channel'])
+            
+            savefig(['C:\Users\Endres Lab\Box Sync\EndresLab\Projects\'...
+                'Optical trapping and imaging\Experiment\Figures\AWG'...
+                '\Power vs Channel Amplitude.fig']);
         end
         
         function startTweezing(obj)
@@ -111,18 +118,22 @@ classdef CharacterizeElectronicsManager < GUIManager
             NFFT = length(discreteWFM.Signal);
             Fs = awg.SamplingRate;
             margin = 1.5 * 10^6;
+            % Impedance of load
+            R = 50; 
             leftBound = freqs(1,1) - margin;
             rightBound = freqs(1, length(freqs)) + margin;
             sa.StartFreq = leftBound / 10^6;
             sa.EndFreq = rightBound / 10^6;
-            
+            f = Fs/2*[-1:2/NFFT:1-2/NFFT];
             % Take what is essentially a DFFT, making sure to give it the
             % exact sampling rate of the awg
-            [pxx,f] = periodogram(discreteWFM.Signal * ...
-                awg.ChAmps(1) / 1000 , [], NFFT, Fs, 'power');
-            
-            % Plot on a log scale
-            plot(f, 10*log10(pxx))
+%             [pxx,f] = periodogram(discreteWFM.Signal * ...
+%                 awg.ChAmps(1) / 1000 , [], NFFT, Fs, 'power');
+            pxx =(fftshift(fft(discreteWFM.Signal * awg.ChAmps(1) / 1000, ...
+                NFFT))/ NFFT);
+            pxx = 10*log10((abs(pxx).^2)/R * 1000);
+            % Plot on a log scaleWW
+            plot(f, pxx)
             axis([leftBound, rightBound, -100, 10])
             title('Theoretical Power Spectrum')
             pause(1)
